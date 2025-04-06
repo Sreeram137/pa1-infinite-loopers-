d9c5e7b7b81e7c6dd8fdb9419386e41c45f66600
Harsha Vardhan Meka <116478108+harshava31@users.noreply.github.com>
2025-04-06 15:41:21 -0400

    Update synch.h

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -41,7 +41,7 @@ void cond_wait (struct condition *, struct lock *);
 void cond_signal (struct condition *, struct lock *);
 void cond_broadcast (struct condition *, struct lock *);
 
-bool cmp_priority(const struct list_elem *, const struct list_elem *, void *);
+bool cmp_cond_priority(const struct list_elem *first, const struct list_elem *second, void *aux);
 
 /* Optimization barrier.
 
