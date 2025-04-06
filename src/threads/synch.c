2b01cdf0eeed433f7b318ec7725ac292c440956c
Harsha Vardhan Meka <116478108+harshava31@users.noreply.github.com>
2025-04-06 15:27:16 -0400

    Update synch.c

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -114,11 +114,13 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters))
-  {
-    list_sort (&sema->waiters, cmp_priority, NULL);
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                struct thread, elem));
+  if (!list_empty(&sema->waiters)) {
+  list_sort(&sema->waiters, cmp_priority, NULL);
+  struct list_elem *e = list_pop_front(&sema->waiters);
+  struct thread *next = list_entry(e, struct thread, elem);
+  thread_unblock(next);
+}
+
   }
   sema->value++;
   intr_set_level (old_level);
